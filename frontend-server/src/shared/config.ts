b1428c12af432a518d461226b0b61321aa33012a
Konstantin Solovev <kostyasolovyov54@gmail.com>
2024-05-21 23:00:38 +0200

    wip

--- frontend-server/src/shared/config.ts
+++ frontend-server/src/shared/config.ts
@@ -1,12 +1,45 @@
-const config = {
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-  AUTH_PRIVATE_KEY: process.env.AUTH_PRIVATE_KEY,
-  AUTH_PASSPHRASE: process.env.AUTH_PASSPHRASE,
-  AUTH_ALGORITHM: 'RS256',
-  BACKEND_URL: process.env.BACKEND_URL,
-  BACKEND_TOKEN: process.env.BACKEND_TOKEN,
-} as const satisfies typeof process.env;
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const config =
+  process.env.NODE_ENV !== 'production'
+    ? ({
+        NODE_ENV: process.env.NODE_ENV,
+        PORT: process.env.PORT,
+        AUTH_PRIVATE_KEY: process.env.AUTH_PRIVATE_KEY,
+        AUTH_PASSPHRASE: process.env.AUTH_PASSPHRASE,
+        AUTH_ALGORITHM: 'RS256',
+        BACKEND_URL: process.env.BACKEND_URL,
+        BACKEND_TOKEN: process.env.BACKEND_TOKEN,
+      } as const satisfies typeof process.env)
+    : await (async () => {
+        try {
+          const [AUTH_PRIVATE_KEY, AUTH_PASSPHRASE] = await Promise.all(
+            [process.env.AUTH_PRIVATE_KEY, process.env.AUTH_PASSPHRASE].map(
+              async (secretPath) =>
+                fs.readFile(path.normalize(secretPath), { encoding: 'utf8' }),
+            ),
+          );
+
+          return {
+            NODE_ENV: process.env.NODE_ENV,
+            PORT: process.env.PORT,
+            AUTH_PRIVATE_KEY,
+            AUTH_PASSPHRASE,
+            AUTH_ALGORITHM: 'RS256',
+            BACKEND_URL: process.env.BACKEND_URL,
+            BACKEND_TOKEN: process.env.BACKEND_TOKEN,
+          } as const satisfies typeof process.env;
+        } catch (error) {
+          if (error instanceof Error) {
+            console.error(new Error(`Cannot read secret: ${error.message}`));
+          } else {
+            console.error(new Error(`Cannot read secret: ${error}`));
+          }
+
+          process.exit(1);
+        }
+      })();
 
 for (const [key, value] of Object.entries(config)) {
   if (value !== undefined) {
--- task-manager/src/tasks/periodicallyLog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "start time: $(date +'%Y-%m-%d %H:%M:%S')"
-
-echo Periodically log
-
-echo "end time: $(date +'%Y-%m-%d %H:%M:%S')"
