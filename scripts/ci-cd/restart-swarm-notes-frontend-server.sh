cc19e574e690696bc2a24205bad0a325eb736972
Konstantin Solovev <kostyasolovyov54@gmail.com>
2024-08-05 18:21:38 +0200

    development

--- scripts/ci-cd/restart-swarm-notes-frontend-server.sh
+++ scripts/ci-cd/restart-swarm-notes-frontend-server.sh
@@ -1,3 +1,7 @@
 docker service update \
-    --force \
+    --image kallenju/notes-frontend-server:latest \
     swarm-notes-frontend-server
+
+docker service update \
+    --force \
+    swarm-notes-nginx-reversive-proxy
--- scripts/ci-cd/update-swarm-notes-backend.sh
+++ scripts/ci-cd/update-swarm-notes-backend.sh
@@ -1,3 +1,7 @@
 docker service update \
     --image kallenju/notes-backend:latest \
     swarm-notes-backend
+
+docker service update \
+    --force \
+    swarm-notes-nginx-reversive-proxy
